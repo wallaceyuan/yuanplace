1539b27e28a2e4df72dd766cbe23f0bf299634f4
wallaceyuan <1272864289@qq.com>
2017-01-16 17:28:21 +0800

    redis auth

--- config/conf.js
+++ config/conf.js
@@ -5,17 +5,6 @@
 var mysql = require('mysql');
 var wrapper = require('co-mysql')
 
-/*var client = require("redis").createClient(6379, "knews-redis2.nrm01e.ng.0001.cnn1.cache.amazonaws.com.cn");
- var ip = 'http://danmaku.kankanews.com';
- var host = 'knews-redis2.nrm01e.ng.0001.cnn1.cache.amazonaws.com.cn';
- var pool = mysql.createPool({
- host:'kankanewsapi.cjspd4t43dgd.rds.cn-north-1.amazonaws.com.cn',
- user:'kankanewsapi',
- password:'kankanewsaws2016',
- database:'kk_danmaku',
- connectTimeout:30000
- });*/
-
 var ip = 'http://127.0.0.1:3000';
 var host = 'localhost';
 var pool = mysql.createPool({
@@ -31,7 +20,12 @@ var ACCESS_KEY = 'hnFPlvlB8QmBiBen8XTa8KEZ_soaiNbzpscllVvv'
 var SECRET_KEY = 'dWGPV89YN8eHM59qF4ehaahpWbz2FtvF65NVnPob'
 var bucket     = 'wallaceyuan'
 
-var client   = require("redis").createClient();
+/*var client   = require("redis").createClient();*/
+
+/*
+var client = require('redis').createClient();
+client.auth('wallace741130')
+*/
 
 
 module.exports = {
